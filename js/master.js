6e201e18fbd3328b52376dfc575e25b6e31cc2de
Jason Noxon <jason@palindromecreative.com>
2019-04-04 20:50:03 -0400

    Updated JS to dynamically grab data

--- js/master.js
+++ js/master.js
@@ -1,55 +1,32 @@
 $(document).ready( function() {
 
-  let weather = [
-    { date: "4/1/2019",
-      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
-      high: "60º",
-      low: "45º"
-    },
-    { date: "4/2/2019",
-      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
-      high: "55º",
-      low: "45º"
-    },
-    { date: "4/3/2019",
-      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
-      high: "73º",
-      low: "45º"
-    },
-    { date: "4/4/2019",
-      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
-      high: "80º",
-      low: "45º"
-    },
-    { date: "4/5/2019",
-      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
-      high: "60º",
-      low: "45º"
-    },
-    { date: "4/6/2019",
-      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
-      high: "24º",
-      low: "45º"
-    },
-    { date: "4/7/2019",
-      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
-      high: "0º",
-      low: "45º"
-    }
-  ];
+  let url = "http://api.apixu.com/v1/forecast.json?key=cc9c36b492994ba8993180021193103&q=12866&days=7";
 
-  $.each (weather, function(index, forecast) {
-    let $highLowH3 = $("<h3/>", { text: forecast.high + "/" + forecast.low });
-    let $weatherImg = $("<img/>", { src: forecast.imgUrl })
-    let $dateH3 = $("<h3/>", { text: forecast.date } );
-    let $article = $("<article/>", { class: "col-1" } );
+  $.ajax(
+   {
+     url: url,
+     success: function(result){
+       let forecast = result.forecast.forecastday;
 
-    $article.append($dateH3);
-    $article.append($weatherImg);
-    $article.append($highLowH3);
+       console.log(forecast);
 
-    $(".cards").append($article);
-  })
+       $.each (forecast, function(index, dayOfWeek) {
+         let $highLowH3 = $("<h3/>", { text: dayOfWeek.day.maxtemp_f + "/" + dayOfWeek.day.mintemp_f });
+         let $weatherImg = $("<img/>", { src: "https:" + dayOfWeek.day.condition.icon })
+         let $dateH3 = $("<h3/>", { text: dayOfWeek.date } );
+         let $article = $("<article/>", { class: "col-1" } );
+
+         $article.append($dateH3);
+         $article.append($weatherImg);
+         $article.append($highLowH3);
+
+         $(".cards").append($article);
+       });
+
+     }
+   });
+
+
 
 });
 
@@ -75,3 +52,41 @@ $(document).ready( function() {
 //       });
 //     }
 //   });
+
+// let weather = [
+//   { date: "4/1/2019",
+//     imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+//     high: "60º",
+//     low: "45º"
+//   },
+//   { date: "4/2/2019",
+//     imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+//     high: "55º",
+//     low: "45º"
+//   },
+//   { date: "4/3/2019",
+//     imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+//     high: "73º",
+//     low: "45º"
+//   },
+//   { date: "4/4/2019",
+//     imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+//     high: "80º",
+//     low: "45º"
+//   },
+//   { date: "4/5/2019",
+//     imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+//     high: "60º",
+//     low: "45º"
+//   },
+//   { date: "4/6/2019",
+//     imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+//     high: "24º",
+//     low: "45º"
+//   },
+//   { date: "4/7/2019",
+//     imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+//     high: "0º",
+//     low: "45º"
+//   }
+// ];
