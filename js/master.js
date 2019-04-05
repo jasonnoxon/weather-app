065b1b76b067c6bda24f559abf0181e8c7d3e704
Jason Noxon <jason@palindromecreative.com>
2019-04-04 20:13:46 -0400

    Added test weather object

--- js/master.js
+++ js/master.js
@@ -1,14 +1,55 @@
 $(document).ready( function() {
-  let $highLowH3 = $("<h3/>", { text: "60º/45º" });
-  let $weatherImg = $("<img/>", { src: "https://cdn.apixu.com/weather/64x64/night/113.png"})
-  let $dateH3 = $("<h3/>", { text: "1/15/2019"} );
-  let $article = $("<article/>", { class: "col-1" } );
 
-  $article.append($dateH3);
-  $article.append($weatherImg);
-  $article.append($highLowH3);
+  let weather = [
+    { date: "4/1/2019",
+      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+      high: "60º",
+      low: "45º"
+    },
+    { date: "4/2/2019",
+      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+      high: "55º",
+      low: "45º"
+    },
+    { date: "4/3/2019",
+      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+      high: "73º",
+      low: "45º"
+    },
+    { date: "4/4/2019",
+      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+      high: "80º",
+      low: "45º"
+    },
+    { date: "4/5/2019",
+      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+      high: "60º",
+      low: "45º"
+    },
+    { date: "4/6/2019",
+      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+      high: "24º",
+      low: "45º"
+    },
+    { date: "4/7/2019",
+      imgUrl: "https://cdn.apixu.com/weather/64x64/night/113.png",
+      high: "0º",
+      low: "45º"
+    }
+  ];
 
-  $(".cards").append($article);
+  $.each (weather, function(index, forecast) {
+    let $highLowH3 = $("<h3/>", { text: forecast.high + "/" + forecast.low });
+    let $weatherImg = $("<img/>", { src: forecast.imgUrl })
+    let $dateH3 = $("<h3/>", { text: forecast.date } );
+    let $article = $("<article/>", { class: "col-1" } );
+
+    $article.append($dateH3);
+    $article.append($weatherImg);
+    $article.append($highLowH3);
+
+    $(".cards").append($article);
+  })
 
 });
 
